19785644e6ca5d1523812a86333de08787446db0
Jer Warren <jerwarren@gmail.com>
2020-04-27 19:03:42 -0700

    cache more stuff

--- sw.js
+++ sw.js
@@ -2,6 +2,14 @@ self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open('sw-cache').then(function(cache) {
 			return cache.add('index.html');
+			return cache.addAll([
+        'index.html',
+        'BellDing-4.27.20-bg.svg',
+        'school-bell.mp3',
+        'script.js',
+				'styles.css',
+				'logo.png'
+      ]);
 		})
 	);
 });
